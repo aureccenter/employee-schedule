540f3c0ccc392d08751eb019924c9ac53777b0d6
princetojo <trevon@andrews.edu>
2021-01-19 10:40:54 -0500

    Removed Routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
-import {Route, HashRouter as Router, Switch} from "react-router-dom";
 import Home from './components/Home';
 import Schedule from './components/Schedule';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/employee-schedule" component={Schedule}/>
-      </Switch>
-    </Router>
+    <div>
+      <Schedule/>
+    </div>
   );
 }
 
