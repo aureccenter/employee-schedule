fa95f5b68cadfff10584616a077f35239f560b25
princetojo <trevon@andrews.edu>
2021-01-19 10:37:26 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, HashRouter as Router, Switch} from "react-router-dom";
+import Home from './components/Home';
+import Schedule from './components/Schedule';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/employee-schedule" component={Schedule}/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Schedule.js
@@ -0,0 +1,9 @@
+const Schedule = () => {
+    return (
+        <div>
+            <h1> This is the Employee Schedule Page </h1>
+        </div>
+    );
+}
+
+export default Schedule;
